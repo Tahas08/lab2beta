afca9bcd13b5ceb55a2226a9df6c7401e4ff6dbd
Tahas08 <92304897+Tahas08@users.noreply.github.com>
2021-11-01 13:10:39 +0200

    Update beta.js

--- beta.js
+++ beta.js
@@ -1,6 +1,9 @@
 window.addEventListener("deviceorientation", on_device_orientation);
-function on_device_orientation(evt){var alpha = evt.alpha;
-var beta = evt.beta;var gamma = evt.gamma;
+function on_device_orientation(evt)
+{
+  var alpha = evt.alpha;
+var beta = evt.beta;
+  var gamma = evt.gamma;
 document.getElementById("a").innerHTML = "alpha = "+alpha;
 document.getElementById("b").innerHTML = "beta = "+beta;
 document.getElementById("c").innerHTML = "gamma = "+gamma;
